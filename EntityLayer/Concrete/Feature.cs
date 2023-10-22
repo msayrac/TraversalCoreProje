c4c76ab76ad93dd875fa3f1b1a9ebbe1b2af55ff
msayrac <msayracode@gmail.com>
2023-10-22 23:00:15 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public string Post1 { get; set; }
+        public string Post1Name { get; set; }
+        public string Post1Description { get; set; }
+        public string Post1Image { get; set; }
+        public bool Post1Status { get; set; }
+
+
+
+    }
+}
