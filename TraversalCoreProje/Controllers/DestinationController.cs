533d007adc79f0e308fef2599ea0fa763bf28964
msayrac <msayracode@gmail.com>
2023-10-30 22:54:56 +0300

    new layout added

--- TraversalCoreProje/Controllers/DestinationController.cs
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Controllers
@@ -14,5 +15,24 @@ namespace TraversalCoreProje.Controllers
 
             return View(values);
         }
+
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+
+            return View();
+
+
+        }
+
+
+
+
 	}
 }
