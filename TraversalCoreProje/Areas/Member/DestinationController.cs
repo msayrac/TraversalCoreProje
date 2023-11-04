ea3480400fb2e08b93ea8aacf429e60cb352ee77
msayrac <msayracode@gmail.com>
2023-11-04 13:30:31 +0300

    Signin destination  layer added

---
+++ TraversalCoreProje/Areas/Member/DestinationController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member
+{
+	[AllowAnonymous]
+	[Area("Member")]
+	public class DestinationController : Controller
+	{
+		DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+		public IActionResult Index()
+		{
+			var values = destinationManager.TGetList();
+
+
+			return View(values);
+		}
+	}
+}
