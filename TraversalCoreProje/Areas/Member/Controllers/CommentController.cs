106f20c72deafd54f2f07996948988d05ad56312
msayrac <msayracode@gmail.com>
2023-11-04 12:18:37 +0300

    Login Layer added

---
+++ TraversalCoreProje/Areas/Member/Controllers/CommentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+	public class CommentController : Controller
+	{
+
+		[Area("Member")]
+		[AllowAnonymous]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
