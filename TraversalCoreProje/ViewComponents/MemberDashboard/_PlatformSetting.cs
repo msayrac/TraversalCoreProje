031ebc2b7175b462aa39131e2964f943a4e49941
msayrac <msayracode@gmail.com>
2023-11-09 23:12:28 +0300

    Platform setting added

---
+++ TraversalCoreProje/ViewComponents/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.MemberDashboard
+{
+	public class _PlatformSetting : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			
+
+			return View();
+		}
+
+
+	}
+}
