bb1135b9d8c767e8c8d22c380ef8e07b2a12cd4f
msayrac <msayracode@gmail.com>
2023-11-11 12:44:18 +0300

    Add Destination Layer is created

--- TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
@@ -1,13 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Areas.Admin.Controllers
 {
 	[Area("Admin")]
 	public class DestinationController : Controller
 	{
+		DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
 		public IActionResult Index()
 		{
+			var values = destinationManager.TGetList();
+			return View(values);
+		}
+
+		[HttpGet]
+		public IActionResult AddDestination()
+		{
+
 			return View();
 		}
+
+		[HttpPost]
+		public IActionResult AddDestination(Destination destinaiton)
+		{
+			destinationManager.TAdd(destinaiton);
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult DeleteDestination(int id)
+		{
+
+			var values = destinationManager.TGetByID(id);
+			destinationManager.TDelete(values);
+			return RedirectToAction("Index");
+
+		}
+
+		[HttpGet]
+		public IActionResult UpdateDestination(int id)
+		{
+			var values = destinationManager.TGetByID(id);
+
+			return View(values);
+
+		}
+
+		[HttpPost]
+		public IActionResult UpdateDestination(Destination destination)
+		{
+			destinationManager.TUpdate(destination);
+
+			return RedirectToAction("Index");
+
+		}
+
+
 	}
 }
