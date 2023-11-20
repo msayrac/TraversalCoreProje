fef827e236bd4844411c0f725d18f9d52184b444
msayrac <msayracode@gmail.com>
2023-11-20 21:55:38 +0300

    destination is added and destination list created with ajax

--- TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TraversalCoreProje.Models;
 
@@ -7,40 +9,71 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
     [Area("Admin")]
     public class CityController : Controller
     {
-        public IActionResult Index()
+        private readonly IDestinationService _destinationService;
+
+		public CityController(IDestinationService destinationService)
+		{
+			_destinationService = destinationService;
+		}
+
+		public IActionResult Index()
         {
             return View();
         }
 
         public IActionResult CityList()
         {
-            var jsonCity = JsonConvert.SerializeObject(cities);
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
             return Json(jsonCity);
         }
 
-        public static List<CityClass> cities = new List<CityClass>
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination destination)
         {
-            new CityClass
-            {
-                CityID=1,
-                CityName = "Üsküp",
-                CityCountry = "Makedonya"
-            },
+            //destination.Status = "true";
 
-            new CityClass
-            {
-                CityID=2,
-                CityName = "Roma",
-                CityCountry = "İtalya"
-            },
+			_destinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination);
+            
+            return Json(values);
 
-            new CityClass
-            {
-                CityID =3,
-                CityName = "Londra",
-                CityCountry = " İngiltere"
-            }
-        };
+        }
 
-    }
+        public IActionResult GetById(int DestinationID)
+        {
+            var values = _destinationService.TGetByID(DestinationID);
+            var jsonValues = JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+
+        }
+
+
+
+
+        //public static List<CityClass> cities = new List<CityClass>
+        //{
+        //    new CityClass
+        //    {
+        //        CityID=1,
+        //        CityName = "Üsküp",
+        //        CityCountry = "Makedonya"
+        //    },
+
+        //    new CityClass
+        //    {
+        //        CityID=2,
+        //        CityName = "Roma",
+        //        CityCountry = "İtalya"
+        //    },
+
+        //    new CityClass
+        //    {
+        //        CityID =3,
+        //        CityName = "Londra",
+        //        CityCountry = " İngiltere"
+        //    }
+        //};
+
+		
+	}
 }
