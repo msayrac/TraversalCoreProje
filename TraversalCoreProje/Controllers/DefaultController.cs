f8bf7efd1b24e59488fbdfbdc146b83504349a49
msayrac <msayracode@gmail.com>
2023-12-02 21:29:04 +0300

    Traversal Home page updated

--- TraversalCoreProje/Controllers/DefaultController.cs
+++ TraversalCoreProje/Controllers/DefaultController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Controllers
 {
+
     public class DefaultController : Controller
     {
+
+        [AllowAnonymous]
+
+
+
         public IActionResult Index()
         {
             return View();
